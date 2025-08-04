24f6dcfd9de499b84b676a2cc4ef6ae1c00d2a6a
mrafee71 <muhammadrafeequeph@gmail.com>
2025-08-04 12:46:11 +0530

    Navbar progressing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import {useState} from 'react'
 import { Outlet, NavLink } from "react-router";
 import { FaRegUser } from "react-icons/fa";
 import { MdShoppingCart } from "react-icons/md";
-import Logo from '../assets/images/logo.png'
+import { RiMenuFoldFill, RiMenuFold2Fill } from "react-icons/ri";
+import Logo from '../assets/images/logo.png';
 
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
   return (
-    <div className='bg-primary text-white px-2 sm:px-4 md:px-8 lg:px-12 xl:px-16'>
+    <div className='relative bg-primary text-white px-2 sm:px-4 md:px-8 lg:px-12 xl:px-16'>
         <nav className='flex justify-between items-center py-4'>
             <div>
-                <img src={Logo} alt="" className='w-20 h-20' />
+                <img src={Logo} alt="" className='w-16 h-16' />
             </div>
             <div className='hidden lg:flex bg-white py-2  px-12 rounded-4xl justify-between'>
                 <NavLink className={({ isActive }) => `px-4 py-0.5 me-4 rounded-xl transition duration-300 ${isActive ? 'text-white bg-primary' : 'text-primary hover:bg-primary hover:text-white'}`} to="/">Home</NavLink>
@@ -20,10 +28,25 @@ const Navbar = () => {
             </div>
             <div className='flex justify-between items-center'>
                 <FaRegUser className='text-2xl me-4' />
-                <MdShoppingCart className='text-3xl bg-white text-primary rounded-4xl p-1' />
+                <MdShoppingCart className='text-3xl me-4 lg:me-0 bg-white text-primary rounded-4xl p-1' />
+                {
+                    isOpen ? (
+                        <RiMenuFold2Fill onClick={toggleMenu} className='text-2xl lg:hidden' />
+                    ) : (
+                        <RiMenuFoldFill onClick={toggleMenu} className='text-2xl lg:hidden' />
+                    )
+                }
             </div>
-          
         </nav>
+
+        {isOpen && (
+            <div className='relative w-full top-0 left-0 right-0 lg:hidden flex justify-around items-center bg-white text-primary py-4 rounded-xl transition-all duration-300'>
+                <NavLink className={({ isActive }) => `px-2 md:px-4 py-0.5 rounded-xl transition duration-300 ${isActive ? 'text-white bg-primary' : 'text-primary hover:bg-primary hover:text-white'}`} to="/">Home</NavLink>
+                <NavLink className={({ isActive }) => `px-2 md:px-4 py-0.5 rounded-xl transition duration-300 ${isActive ? 'text-white bg-primary' : 'text-primary hover:bg-primary hover:text-white'}`} to="/shop">Shop</NavLink>
+                <NavLink className={({ isActive }) => `px-2 md:px-4 py-0.5 rounded-xl transition duration-300 ${isActive ? 'text-white bg-primary' : 'text-primary hover:bg-primary hover:text-white'}`} to="/about">About</NavLink>
+                <NavLink className={({ isActive }) => `px-2 md:px-4 py-0.5 rounded-xl transition duration-300 ${isActive ? 'text-white bg-primary' : 'text-primary hover:bg-primary hover:text-white'}`} to="/contact">Contact</NavLink>
+            </div>      
+        )}
 
         <Outlet />
     </div>
